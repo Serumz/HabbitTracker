af38ca6033a933361a97b9f029a8cd7a59316537
EnigmaTraveller <73563149+EnigmaTraveller@users.noreply.github.com>
2021-02-20 17:54:04 +0000

    Updated Login.txs
    Changed fetch POST code

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,12 +6,16 @@ export const Login: React.SFC<{}> = () => {
     const [usernameValue, setUsername] = useState("");
     const [passwordValue, setPassword] = useState("");
     let userDetails = new User(usernameValue, passwordValue);
+
     return (
       <div>
         <form onSubmit={function(event) {
                 event.preventDefault();
-                fetch({url: "localhost:5000/users/login", method: "POST", body: })
-                .then()
+                fetch('httpslocalhost:5000/users/login', {
+                  method: "POST",
+                  body: JSON.stringify({username: usernameValue, password: passwordValue})
+                })
+                .then();
             }}>
               <label><b>Username</b></label>
                 <input type="text" placeholder="Enter Username" value={usernameValue} onChange={function(event) {
